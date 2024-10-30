b9c634707f81b1b5a0716fe8a30290ce9372c8d4
le hoang nam <lehoangnam.31123@gmail.com>
2024-10-30 15:39:22 +0700

    Update UI HomePage: Add button Back to Top

--- javascript/scrollFixed.js
+++ javascript/scrollFixed.js
@@ -3,10 +3,12 @@ window.onscroll = function() {myFunction()};
 function myFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("nav-fixed").style.top = 0;
+    document.querySelector(".back-to-top").style.display="inline-block" ;
   } else {
     document.getElementById("contactID").style.position = "relative"
     document.getElementById("contactID").style.top = 0;
     document.getElementById("contactID").style.zIndex = 9999;
     document.getElementById("nav-fixed").style.top = "40px";
+    document.querySelector(".back-to-top").style.display="none" ;
   }
 }
